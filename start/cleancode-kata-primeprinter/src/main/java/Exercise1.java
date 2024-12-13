1ce7b64960c4b35c84de6d8ff501ffc19b2e377f
Dylan Zakaharinivo <zakadylan2005@gmail.com>
2024-12-13 10:48:19 +0100

    exercise1 simplification improved

--- start/cleancode-kata-primeprinter/src/main/java/Exercise1.java
+++ start/cleancode-kata-primeprinter/src/main/java/Exercise1.java
@@ -14,13 +14,8 @@ public class Exercise1 {
 
     public boolean isEdible()
     {
-        if (this.expirationDate.isAfter(LocalDate.now()) &&
+        return (this.expirationDate.isAfter(LocalDate.now()) &&
                 this.approvedForConsumption == true &&
-                this.inspectorId != null)
-        {
-            return true;
-        }
-        return false;
-
+                this.inspectorId != null);
     }
 }
