5356691cc645632f4ffff9bc879759a26178354e
Dylan Zakaharinivo <zakadylan2005@gmail.com>
2024-12-13 10:56:47 +0100

    exercise3 replaced if else if by switch case

--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/Exercise3.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/Exercise3.java
@@ -1,4 +1,25 @@
-package com.cleancode.knuth;
-
 public class Exercise3 {
+    public double calculate(double amount, int type, int years)
+    {
+        double result = 0;
+        double disc = (years > 5) ? (double) 5 / 100 : (double)years / 100;
+
+        switch(type){
+            case 1:
+                result = amount;
+                return result;
+            case 2:
+                result = (amount - (0.1 * amount)) - disc * (amount - (0.1 * amount));
+                break;
+            case 3:
+                result = (0.7 * amount) - disc * (0.7 * amount);
+                return result;
+                break;
+            case 4:
+                result = (amount - (0.5 * amount)) - disc * (amount - (0.5 * amount));
+                break;
+            default:
+                return result;
+        }
+    }
 }
